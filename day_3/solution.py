062da35c5b4d6da6eb4db53778f248c7195fbad1
Joseph Haaga <haaga.joe@gmail.com>
2022-12-03 19:20:53 -0500

    part 1

---
+++ day_3/solution.py
@@ -0,0 +1,38 @@
+from typing import Tuple
+
+
+# split knapsack into compartments
+def split_knapsack(knapsack: str) -> Tuple[str, str]:
+    l = len(knapsack) // 2
+    return (knapsack[:l], knapsack[l:])
+
+# find common item (set intersection) of two compartments
+# {}.intersection({})
+
+# calculate priority of item
+def calculate_priority(item: str) -> int:
+    root, intercept  = ("A", 27) if item.isupper() else ("a", 1)
+    return (ord(item) - ord(root)) + intercept
+
+
+
+def main() -> int:
+    total: int = 0
+    with open("input.txt", "r") as f:
+        lines = f.read().split("\n")
+    for line in lines:
+        pocket_one, pocket_two = split_knapsack(line)
+        try:
+            duplicate_item = set(pocket_one).intersection(set(pocket_two)).pop()
+        except KeyError:
+            continue
+        priority = calculate_priority(duplicate_item)
+        print(priority)
+        total += priority
+    # part 1
+    print(total)
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
