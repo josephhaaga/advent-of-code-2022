975a6b4b72f2cfebd4906f2ced1b3c250157deb9
Joseph Haaga <haaga.joe@gmail.com>
2022-12-11 11:43:46 -0500

    working on day 9 still

--- day_9/solution.py
+++ day_9/solution.py
@@ -13,7 +13,7 @@ def get_board_size(instructions: str) -> int:
     return max([int(line.split(" ")[1]) for line in instructions.split("\n")[:-1]])
 
 
-def print_board(board_size: int, head_location: Optional[HeadLocation] = None) -> None:
+def print_board(board_size: int, head_location: Optional[Location] = None) -> None:
     for y_idx in range(board_size):
         for x_idx in range(board_size):
             print("." if (x_idx, y_idx) != head_location else "H", end=" ")
@@ -27,34 +27,26 @@ def update_location(
     return Location(location.x + move.x, location.y + move.y)
 
 
-def instruction_to_move(instruction: str) -> Move:
+def instruction_to_moves(instruction: str) -> List[Move]:
     direction, distance = instruction.split(" ")
     distance = int(distance)
-    if direction.lower() == "U":
-        return Move(-1 * distance, 0)
-    elif direction.lower() == "D":
-        return Move(distance, 0)
-    elif direction.lower() == "L":
-        return Move(0, -1 * distance)
-    elif direction.lower() == "R":
-        return Move(0, distance)
-    else:
-        raise ValueError(f'direction {direction} unrecognized!')
+    dirs = {
+        "U": (-1, 0),
+        "D": (1, 0),
+        "L": (0, -1),
+        "R": (0, 1)
+    }
+    return [Move(*dirs[direction]) for _ in range(distance)]
 
 
-for line in instructions.split("\n")[:-1]:
-    move = instruction_to_move(line)
-    # moves: List[Move]
+def calculate_distance(a: Location, b: Location) -> Tuple[int, int]:
+    """Calculate the [x, y] distance between `a` and `b`."""
+    return (b.x-a.x, b.y-a.y)
 
-    head = update_location(head, move)
-    # for move in moves:
-    #   head = update_location(head, move)
-    #   if calculate_distance(tail, head) > 1:
-    #       tail = update_location(tail, get_best_move(head, tail))
 
-    if distance(head, tail) > 1:
-        tail = update_location(tail, get_best_move(head, tail))
-        # we need to update tail and head at the same time, since a direction could have distance 4
+def get_best_move(tail: Location, distance: Tuple[int, int]) -> Move:
+    """Returns the Move that makes `tail` adjacent to `head`."""
+    ...
 
 
 # How many positions does the tail of the rope visit at least once?
@@ -65,6 +57,20 @@ def main() -> int:
         instructions = f.read()
     size = get_board_size(instructions)
     print_board(size, (4, 4))
+    # Part 1
+    for line in instructions.split("\n")[:-1]:
+        moves: List[Move] = instruction_to_moves(line)
+        for move in moves:
+          head: Location = update_location(head, move)
+          distance: Tuple[int, int] = calculate_distance(tail, head) # (0, 1)
+          if distance[0]:
+              tail = update_location(tail, get_best_move(head, tail))
+
+"""
+if -1 < distance[0] < 1 and -1 < distance[1] < 1:
+    print("tail is adjacent to head")
+"""
+
     return 0
 
 
