aa4040f888dae8e8a19a85a3ee07e5f75b03ba0b
Joseph Haaga <haaga.joe@gmail.com>
2022-12-12 19:01:21 -0500

    finish day 9 part 1

--- day_9/solution.py
+++ day_9/solution.py
@@ -5,6 +5,7 @@ from typing import Optional
 from typing import Tuple
 
 
+Distance = namedtuple("Distance", "x y")
 Move = namedtuple("Move", "x y")
 Location = namedtuple("Location", "x y")
 
@@ -39,14 +40,23 @@ def instruction_to_moves(instruction: str) -> List[Move]:
     return [Move(*dirs[direction]) for _ in range(distance)]
 
 
-def calculate_distance(a: Location, b: Location) -> Tuple[int, int]:
+def calculate_distance(a: Location, b: Location) -> Distance:
     """Calculate the [x, y] distance between `a` and `b`."""
-    return (b.x-a.x, b.y-a.y)
+    return Distance(b.x-a.x, b.y-a.y)
 
 
-def get_best_move(tail: Location, distance: Tuple[int, int]) -> Move:
+def get_best_move(tail: Location, distance: Distance) -> Move:
     """Returns the Move that makes `tail` adjacent to `head`."""
-    ...
+    delta_x, delta_y = distance.x, distance.y # 2, -2
+    if delta_x < 0:
+        delta_x = -1
+    elif delta_x > 1:
+        delta_x = 1
+    if delta_y < 0:
+        delta_y = -1
+    elif delta_y > 1:
+        delta_y = 1
+    return Move(delta_x, delta_y)
 
 
 # How many positions does the tail of the rope visit at least once?
@@ -58,18 +68,20 @@ def main() -> int:
     size = get_board_size(instructions)
     print_board(size, (4, 4))
     # Part 1
+    tail_locations = []
+    head = Location(20, 20)
+    tail = Location(20, 20)
+    tail_locations += [tail]
     for line in instructions.split("\n")[:-1]:
         moves: List[Move] = instruction_to_moves(line)
         for move in moves:
-          head: Location = update_location(head, move)
-          distance: Tuple[int, int] = calculate_distance(tail, head) # (0, 1)
-          if distance[0]:
-              tail = update_location(tail, get_best_move(head, tail))
-
-"""
-if -1 < distance[0] < 1 and -1 < distance[1] < 1:
-    print("tail is adjacent to head")
-"""
+            head: Location = update_location(head, move)
+            distance: Distance = calculate_distance(tail, head)
+            if not (-1 <= distance[0] <= 1 and -1 <= distance[1] <= 1):
+                best_move: Move = get_best_move(tail, distance)
+                tail: Location = update_location(tail, best_move)
+                tail_locations += [tail]
+    print(len(set(tail_locations)))
 
     return 0
 
