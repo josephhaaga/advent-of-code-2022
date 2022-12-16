872ac63645c73bfd56d540d06a88d1a7a6f5f155
Joseph Haaga <haaga.joe@gmail.com>
2022-12-15 20:23:36 -0500

    finish day 10

--- day_10/solution.py
+++ day_10/solution.py
@@ -9,24 +9,34 @@ def read_instructions(path: str) -> List[str]:
 
 
 def main() -> int:
+    instructions = read_instructions(sys.argv[1])
+    # part 1
     X = 1
     cycle = 0
-    instructions = read_instructions(sys.argv[1])
-    strengths = [X]
+    positions = []
     for instruction in instructions:
         op, *amount = instruction.split(" ")
         if op == "noop":
             cycle += 1
         elif op == "addx":
-            strengths.append(X)
+            positions.append(X)
             cycle += 2
             X += int(amount[0])
-        strengths.append(X)
+        positions.append(X)
+
+    # part 1
     def get_strength_at(y):
-        return y * (strengths[y-1])
+        return y * (positions[y-2])
     print(sum([get_strength_at(x) for x in [20, 60, 100, 140, 180, 220]]))
 
-
+    # part 2
+    for cycle_idx, position in enumerate(positions):
+        if (cycle_idx + 1) % 40 in range(position - 1, position + 2):
+            print("#", end='')
+        else:
+            print(".", end='')
+        if (cycle_idx + 1) % 40 == 0:
+            print()
     return 0
 
 
