501ff7a4aa960eed1b721a07939f99f33ced9d78
Joseph Haaga <haaga.joe@gmail.com>
2022-12-25 12:34:13 -0500

    getting the wrong answer

--- day_11/solution.py
+++ day_11/solution.py
@@ -6,44 +6,71 @@ from typing import Callable
 from typing import List
 
 
-@dataclass
-class Monkey:
-    name: str
-    operation: Callable[int, int]
-    test: Callable[int, Callable]
-    items: List[int] = field(default_factory=list)
-    num_inspections: int = 0
-
-
 name_pattern = re.compile("Monkey (\d+):")
 items_pattern = re.compile("Starting items: ([\d, ]+)")
-operation_pattern = re.compile("Operation: new = (.+)\n")
+operation_pattern = re.compile("Operation: new = (.+)")
 test_pattern = re.compile("Test: divisible by (\d+)")
 true_pattern = re.compile("If true: throw to monkey (\d+)")
 false_pattern = re.compile("If false: throw to monkey (\d+)")
 
+
+@dataclass
+class Monkey:
+    name: str
+    operation: str
+    test: str
+    items: List[int]
+    true: int
+    false: int
+    num_inspections: int = 0
+
+    def give(self, item) -> None:
+        self.items.append(item)
+
+    def inspect_items(self, monkeys) -> None:
+        while len(self.items) > 0:
+            self.num_inspections += 1
+            old = self.items.pop(0)
+            new = eval(self.operation)
+            new /= 3
+            monkeys[
+                self.true
+                if new % int(self.test) == 0
+                else self.false
+            ].give(new)
+        assert len(self.items) == 0
+
+
 def parse_instructions_into_monkeys(filepath: str) -> List[Monkey]:
     with open(filepath, 'r') as f:
         instructions = f.read().split("\n\n")
+    result = {}
     for instruction in instructions:
         lines = instruction.split("\n")
         name = re.search(name_pattern, lines[0]).groups(0)[0]
         items = [int(x) for x in re.search(items_pattern, lines[1]).groups(0)[0].split(", ")]
         operation = re.search(operation_pattern, lines[2]).groups(0)[0]
-        breakpoint()
-    breakpoint()
-    return None
+        test = re.search(test_pattern, lines[3]).groups(0)[0]
+        true = re.search(true_pattern, lines[4]).groups(0)[0]
+        false = re.search(false_pattern, lines[5]).groups(0)[0]
+        monkey = Monkey(name, operation, test, items, true, false)
+        result[monkey.name] = monkey
+    return result
 
 
 def main() -> int:
     # parse instructions into monkeys
-    monkeys: List[Monkey] = parse_instructions_into_monkeys(sys.argv[1])
-
+    monkeys: Mapping[Monkey] = parse_instructions_into_monkeys(sys.argv[1])
     for round_ in range(20):
-        for monkey in monkeys:
-            pass
-            # monkey inspects all items
-    print(product)
+        for i in range(len(monkeys)):
+            name = str(i)
+            monkey = monkeys[name]
+            print(f"Processing monkey {name}")
+            monkey.inspect_items(monkeys)
+    print()
+    x = sorted(monkeys.values(), key=lambda x: x.num_inspections)
+    breakpoint()
+    print(x[-2].num_inspections * x[-1].num_inspections)
     return 0
 
 
